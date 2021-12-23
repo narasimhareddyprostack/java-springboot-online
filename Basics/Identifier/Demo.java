ddaee3b5e48bf8d2ffa36b569797b7e6fee114c7
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-12-23 07:17:18 +0530

    okie

---
+++ Basics/Identifier/Demo.java
@@ -0,0 +1,8 @@
+class Demo{
+    public static void main(String[] args){
+        int size[] = {38, 40 ,42, 44};
+        String names[] = {"Narasimha","Suchitra", "Srikanth"};
+        System.out.println(size[0]);
+        System.out.println(names[0]);
+    }
+}
---
+++ Basics/Identifier/Test.java
@@ -0,0 +1,33 @@
+class Test{
+    public static void main(String[] args){
+        byte bt = 99;
+        short s = 4343;
+        int a = 100;
+        int b = 4390;
+        long l = 452525523;
+        
+        float rating = 4.3f;
+        double rating2 = 4.3;
+        boolean is_Avail = true;
+        char size = 'S';
+        
+        System.out.println(bt);
+        System.out.println(s);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("Long Data Type"+ l);
+        
+        System.out.println(rating);
+        System.out.println(rating2);
+         System.out.println(is_Avail);
+        System.out.println(size);
+    }
+}
+// byte data type - :  -128 to 127
+// short data type - : -32768 to 32767 
+
+/* Int:
+The most commonly used Data Type is "int"
+size: 4 - Bytes 
+Range : -2 power 31 to 2 power 31 -1  */
+
