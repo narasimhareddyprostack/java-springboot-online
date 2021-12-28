081bdffdccb48b38ee64b352a69146d3555951f2
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-12-28 20:06:00 +0530

    java basics

---
+++ part1/Op2/Test.java
@@ -0,0 +1,9 @@
+class Test{
+   public  static void main(String[] args){
+           String s1 = "Hello";
+           String s2 = "Good Morning";
+           s2 = s2.concat(",Welcome to prostack");
+           System.out.println(s1+s2);             
+    }   
+}
+
---
+++ part1/controlflow/cond/Test.java
@@ -0,0 +1,9 @@
+class Test{
+   public  static void main(String[] args){
+           int x = 1000;
+           if(x == 2000){
+                System.out.println("Hello, Good Evening");  
+           }             
+    }   
+}
+
